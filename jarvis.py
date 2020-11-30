f127c0a76537e29a9ee8dd1faf6525e06238b8a5
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2020-11-30 16:19:30 +0530

    imported the packages

---
+++ jarvis.py
@@ -0,0 +1,11 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import smtplib
+import webbrowser as wb
+import os
+import pyautogui 
+import psutil
+import pyjokes
+
