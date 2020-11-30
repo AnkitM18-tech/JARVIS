db332185cd13dd78d1d98a93f1f3825a171862d0
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2020-11-30 16:30:05 +0530

    feat : main function added

--- jarvis.py
+++ jarvis.py
@@ -87,3 +87,76 @@ def cpuUsage():
     
 def tellJokes():
     speak(pyjokes.get_joke())
+
+if __name__ == "__main__":
+    wishme()
+    while True:
+        query=takeCommand().lower()
+        if "time" in query:
+            time()
+            
+        elif "date" in query:
+            date()
+            
+        elif "wikipedia" in query:
+            speak("Searching...")
+            query=query.replace("wikipedia","")
+            result=wikipedia.summary(query,sentences=2)
+            print(result)
+            speak(result)
+            
+        elif "send email" in query:
+            try:
+                speak("What Should I say?")
+                content=takeCommand()
+                to="luckyvk23@gmail.com"
+                sendEmail(to,content)
+                speak("Email has been sent!")
+            except Exception as ex:
+                print(ex)
+                speak("Unable To send The email!")
+                
+        elif "search in chrome" in query:
+             speak("What Should I search?")
+             chromepath="C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+             search=takeCommand().lower()
+             wb.get(chromepath).open_new_tab(search +".com")
+             
+        elif "logout" in query:
+            os.system("shutdown -l")
+            
+        elif "shutdown" in query:
+            os.system("shutdown /s /t 1")
+            
+        elif "restart" in query:
+            os.system("shutdown /r /t 1")
+             
+        elif "play songs" in query:
+            songs_dir ="D:\\Mobile Storage\\UCDownloads\\Music"
+            songs=os.listdir(songs_dir)
+            os.startfile(os.path.join(songs_dir,songs[0]))
+            
+        elif "remember that" in query:
+            speak("What should I remember?")
+            data=takeCommand()
+            speak("You told me To Remember "+data)
+            remember=open("data.txt","w")
+            remember.write(data)
+            remember.close()
+            
+        elif "do you know anything" in query:
+            remember=open("data.txt","r")
+            speak("You told me to remember that "+remember.read())
+            
+        elif "screenshot" in query:
+            screenShot()
+            speak("Done!")
+            
+        elif "cpu" in query:
+            cpuUsage()
+            
+        elif "joke" in query:
+            tellJokes()
+            
+        elif "offline" in query:
+            quitScr()
