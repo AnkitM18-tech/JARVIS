0c7cc4078c4147b569b9cca27387ac9dac99c2f3
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2020-11-30 16:21:47 +0530

    feat : initial functions added

--- jarvis.py
+++ jarvis.py
@@ -9,3 +9,38 @@ import pyautogui
 import psutil
 import pyjokes
 
+engine = pyttsx3.init()
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    
+def time():
+    Time=datetime.datetime.now().strftime("%I:%M:%S")
+    speak("The Current time is:")
+    speak(Time)
+    
+def date():
+    year=int(datetime.datetime.now().year)
+    month=int(datetime.datetime.now().month)
+    date=int(datetime.datetime.now().day)
+    speak("The Current Date is:" )
+    speak(date)
+    speak(month)
+    speak(year)
+
+def wishme():
+    hour=datetime.datetime.now().hour   
+    speak("Welcome Back!")
+    time()
+    date()
+    if hour>=6 and hour<12:
+        speak("Good Morning Ankit!")
+    elif hour>=12 and hour<18:
+        speak("Good AfterNoon Ankit!")
+    elif hour>=18 and hour<24:
+        speak("Good Evening Ankit!")
+    else:
+        speak("Good Night Ankit!")
+    speak("JARVIS at your service! Please,tell me How can I Help You?")
